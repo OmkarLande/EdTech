2793e5beb4f413911060825ae49daec6a551c655
kaustubh <kaustubhmayekar02@gmail.com>
2023-06-09 01:39:29 +0530

    created login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Login from './components/Login'
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Login/>  
     </>
   )
 }
