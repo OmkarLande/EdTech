26e031b30e4d7d00cd91471cec5a618da3bc698e
Siddhesh1123 <101471387+Siddhesh1123@users.noreply.github.com>
2023-06-23 23:02:01 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+ 
   theme: {
+     colors: {
+      bg: "#fff",
+      text: "#000",
+      transparent: "#ffffff00",
+      green: "#33BEAF",
+      gray: "#96999E",
+      shadow: "#CDD2DA"
+      
+    },
+   
     extend: {},
   },
   plugins: [],
