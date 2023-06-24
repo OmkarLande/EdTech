ad25ddee276eb1a8e6e50a51c401930f28e16d79
Siddhesh1123 <101471387+Siddhesh1123@users.noreply.github.com>
2023-06-24 23:06:02 +0530

    profile complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { ReactDOM } from 'react'
 import { BrowserRouter ,Routes,Route} from 'react-router-dom'
 import Navbar from './components/Common/Navbar'
 import Login from './pages/Login'
@@ -7,7 +6,8 @@ import Instructorlogin from './pages/Instructorlogin'
 import VerifyEmail from './pages/VerifyEmail'
 import Studentsignin from './pages/Studentsignin'
 import Instructorsignin from './pages/Istructorsignin'
-
+import Studentprofile from './pages/Studentprofile'
+import Instructorprofile from './pages/Instructorprofile'
 import Home from './pages/Home'
 
 function App() {
@@ -21,6 +21,8 @@ function App() {
           <Route path="/Instructorlogin" element={<Instructorlogin />}></Route>
           <Route path="/Studentsignin" element={<Studentsignin />}></Route>
           <Route path="/Instructorsignin" element={<Instructorsignin />}></Route>
+          <Route path="/Studentprofile" element={<Studentprofile />}></Route>
+          <Route path="/Instructorprofile" element={<Instructorprofile />}></Route>
           <Route path="/VerifyEmail" element={<VerifyEmail />}></Route>
         </Routes>
         
