a8f6272b632e4ba53e5b07006f05e5ec51c02f1b
kaustubh <kaustubhmayekar02@gmail.com>
2023-06-25 15:11:41 +0530

    otp verification complete

--- src/App.js
+++ src/App.js
@@ -4,26 +4,15 @@ import { BrowserRouter ,Routes,Route} from 'react-router-dom'
 import Navbar from './components/Common/Navbar'
 import Login from './pages/Login'
 import Instructorlogin from './pages/Instructorlogin'
-<<<<<<< HEAD
 import VerifyEmail from './pages/VerifyEmail'
 
-=======
 import Home from './pages/Home'
->>>>>>> 666be6f286124e307749d4c9d2f04ce1284e9f35
 
 function App() {
   return (
     <>
       <Navbar />
-
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path="/Login" element={<Login />}></Route>
-          <Route path="/Instructorlogin" element={<Instructorlogin />}></Route>
-        </Routes>
-        
-    </BrowserRouter>      
+      <VerifyEmail />
     </>
   )
 }
